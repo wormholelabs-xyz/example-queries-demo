ce5f4d70edf40aa007a1b90c82d4782d9aca4662
Evan Gray <battledingo@gmail.com>
2024-03-21 10:54:37 -0400

    ui: moved from evan-gray/vaa-dev

---
+++ ui/src/index.tsx
@@ -0,0 +1,70 @@
+import {
+  createTheme,
+  CssBaseline,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            overflowY: "scroll",
+          },
+          "*": {
+            scrollbarWidth: "thin",
+            scrollbarColor: `${grey[700]} ${grey[900]}`,
+          },
+          "*::-webkit-scrollbar": {
+            width: "8px",
+            height: "8px",
+            backgroundColor: grey[900],
+          },
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: grey[700],
+            borderRadius: "4px",
+          },
+          "*::-webkit-scrollbar-corner": {
+            // this hides an annoying white box which appears when both scrollbars are present
+            backgroundColor: "transparent",
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: "none",
+          },
+        },
+      },
+      MuiInputBase: {
+        styleOverrides: {
+          input: { fontFamily: "monospace" },
+        },
+      },
+    },
+  }),
+);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
